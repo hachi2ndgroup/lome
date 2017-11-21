246b1fbb1f4ec705132e5c8ce9f53e54172859c7
hachi2nd <aiueo@example.com>
2017-11-22 08:53:27 +0900

    tmpの保存ファイル名にpanoidをつけて、同時に複数のクエリが来ても大丈夫にした

--- getimage.sh
+++ getimage.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 panoid=$1
-
+prefix="tmp/$1"
 for y in 0 1 2 3
 do
-   curl -o "tmp/#1.png" "http://maps.google.com/cbk?output=tile&panoid=$panoid&zoom=3&x=[0-7]&y=$y"
-   convert +append "tmp/0.png" "tmp/1.png" "tmp/2.png" "tmp/3.png" "tmp/4.png" "tmp/5.png" "tmp/6.png" "tmp/7.png" "tmp/h-$y.png"
+   curl -o "${prefix}_#1.png" "http://maps.google.com/cbk?output=tile&panoid=$panoid&zoom=3&x=[0-7]&y=$y"
+   convert +append "${prefix}_0.png" "${prefix}_1.png" "${prefix}_2.png" "${prefix}_3.png" "${prefix}_4.png" "${prefix}_5.png" "${prefix}_6.png" "${prefix}_7.png" "${prefix}_h-$y.png"
 done
- convert -append "tmp/h-0.png" "tmp/h-1.png" "tmp/h-2.png" "tmp/h-3.png" "tmp/image.png"
- convert -crop 3328x1664+0+0  "tmp/image.png"  "image/$panoid.png"
-
+ convert -append "${prefix}_h-0.png" "${prefix}_h-1.png" "${prefix}_h-2.png" "${prefix}_h-3.png" "${prefix}_image.png"
+ convert -crop 3328x1664+0+0  "${prefix}_image.png"  "image/$panoid.png"
+ rm ${prefix}*
